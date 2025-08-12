87158bf5b7820aaec2cf32496a2dec4e7c8d78be
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-08-12 20:56:11 +0530

    string-methods

---
+++ src/stringtopic/immutableway/TestApp1.java
@@ -0,0 +1,116 @@
+package stringtopic.immutableway;
+
+public class TestApp1 
+{
+	public static void main(String[] args) 
+	{
+		String name = "royal";
+
+		System.out.println("name : " + name);
+
+		String value = 	name.repeat(3);
+		System.out.println("value : " + value);
+		
+//		int no1=100;
+//		int no2=200;
+		
+//		String no1Str = String.valueOf(no1);
+//		String no2Str = String.valueOf(no2);		
+//		System.out.println(no1Str+no2Str);
+		
+		
+		//				 0    1  2   3    4    5         6  7     8
+//		String name1 = "This is Java which is Developed by James Gosling";
+
+		// string data ---convert into byte[ASCII]
+		
+//		byte b[] =name1.getBytes();
+//		for (int i = 0; i < b.length; i++) 
+//		{
+//			System.out.println(name1.charAt(i)+"---"+b[i]);
+//		}
+		
+//		char v[] = 	name1.toCharArray();
+//		for (char c : v) {
+//			System.out.println(c);
+//		}
+		
+//		String words[] =	name1.split("\\s");
+//		System.out.println("words.length : "  + words.length);
+//		for (String word : words) 
+//		{
+//			System.out.println(word);
+//		}
+//		System.out.println(name1);
+//		name1= name1.replace("is", "was");
+//		System.out.println(name1);
+		
+//		String value = name1.substring(8);
+//		String value = name1.substring(8,12);// 8-11
+//		System.out.println(value);// 8 to lastIndex
+		
+		
+//		String name1=" royal ";
+//		System.out.println(name1.length());// 7
+//		name1 = name1.trim();// "royal"
+//		System.out.println(name1.length());// 7
+//		String name2="techno";
+		
+//		System.out.println(name1.toLowerCase());
+//		System.out.println(name2.toUpperCase());
+//		String fullname =	name1.concat(name2);
+//		System.out.println("fullname : "  +fullname);
+//		String name1 = "This is Java which is Developed by James Gosling";
+		
+//		int index = name1.indexOf("is");
+
+//		int index = name1.lastIndexOf("is");
+//		System.out.println("index : " + index);
+		
+		
+//		String value = " ";
+//		System.out.println("isEmpty : " + value.isEmpty());// false
+//		System.out.println("isBlank : " + value.isBlank());// true
+		
+//		String name1 = "This is Java which is Developed by James Gosling";
+//		boolean flag = name1.contains("abc");
+//		System.out.println(flag);
+		
+		
+//		String name1="royal";
+//		String name2="royal";
+		
+//		if (name1.equals(name2)) 
+		//if (name1.equalsIgnoreCase(name2)) 
+//		if (name1.compareTo(name2) == 0) 
+//		{
+//			System.out.println("Both strings are same");
+//			
+//		} else 
+//		{
+//			System.out.println("Both strings are not same");
+//		}
+		
+		//			    012345678901
+//		String value = "This is Java";
+		
+//		boolean flag = value.endsWith("this");
+//		boolean flag = value.startsWith("java");
+		
+//		System.out.println(flag);
+		
+		
+//		System.out.println("value.length() : " + value.length());
+
+//		for (int i = 0; i < value.length(); i++) 
+//		{
+//			System.out.println("value.charAt("+i+") : " + value.charAt(i));
+//		}
+//		System.out.println("value.charAt(1) : " + value.charAt(1));
+//		System.out.println("value.charAt(2) : " + value.charAt(2));
+//		System.out.println("value.charAt(3) : " + value.charAt(3));
+//		System.out.println("value.charAt(4) : " + value.charAt(4));
+//		System.out.println("value.charAt(5) : " + value.charAt(5));
+//		System.out.println("value.charAt(6) : " + value.charAt(6));
+	}
+}
